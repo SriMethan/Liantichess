73864d650a3624477f890454e180add076ad0008
TheYoBots <yohaan.nathanjw@gmail.com>
2022-01-23 07:26:31 +0000

    Fix variant rules - Fixes antiatomic ending rule (should be a draw not a win/loss). - Disables castling from all variants (as per Fairy sf giveaway is antichess with castling).

--- client/variantsIni.ts
+++ client/variantsIni.ts
@@ -3,28 +3,40 @@ export const variantsIni = `
 [anti_antichess:giveaway]
 extinctionValue = loss
 stalemateValue = loss
+castling = false
 
 # Hybrid of antichess and atomic
-[antiatomic:giveaway]
-blastOnCapture = true
+# This might look like what you'd call coffeeatomic, but it isn't.
+[antiatomic:atomic]
+mustCapture = true
+stalemateValue = win
+extinctionValue = win
+promotionPieceTypes = nbrqk
+commoner = k
+extinctionPieceTypes = *
+castling = false
 
 # Hybrid of antichess and zh. Antichess is the base variant.
 [antihouse:giveaway]
 pieceDrops = true
 capturesToHand = true
 pocketSize = 6
+castling = false
 
-# Hybrid of antichess and zh
+# Hybrid of antichess and zh. Zh is th base variant.
 [coffeehouse:crazyhouse]
 mustCapture = true
+castling = false
 
 # Hybrid variant of antichess and king of the hill
 [coffeehill:kingofthehill]
 mustCapture = true
+castling = false
 
 # Hybrid variant of antichess, atomic and king of the hill
 [atomic_giveaway_hill:giveaway]
 blastOnCapture = true
 flagPiece = k
 whiteFlag = d4 e4 d5 e5
-blackFlag = d4 e4 d5 e5`
+blackFlag = d4 e4 d5 e5
+castling = false`
